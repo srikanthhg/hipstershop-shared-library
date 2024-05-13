b09e6b7e36a178430ed35cc5233e4c4b9c137679
srikanthg <skanth306@gmail.com>
2024-05-13 16:53:00 +0530

    jenkins

--- vars/pythonEKS.groovy
+++ vars/pythonEKS.groovy
@@ -1,60 +1,62 @@
-pipeline{
-    agent any
+def call(Map configMap){  
+    pipeline{
+        agent any
 
-    options {
-        timeout(time: 1, unit: 'HOURS')
-        disableConcurrentBuilds()
-        ansiColor('xterm')
-    }
-
-    parameters {
-        booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
-    }
-        
-    stages{
-        stage('Clone code from GitHub'){
-            steps{
-               // git branch: 'main', url: 'https://github.com/srikanthhg/checkoutservice'
-               git url: 'https://github.com/srikanthhg/recommendationservice',  branch: 'main'
-            }
+        options {
+            timeout(time: 1, unit: 'HOURS')
+            disableConcurrentBuilds()
+            ansiColor('xterm')
         }
-        stage('Get the version'){
-            steps{
-                script {
-                    def packageVersion = readFile('version.py').trim()
-                    if (packageVersion) {
-                        // Access the captured version group (group 1)
-                        //packageVersion = packageVersion[0][1]
-                        echo "Application version: $packageVersion"
-                    } else {
-                        error "Application version not found in main.go"
-                    }          
+
+        parameters {
+            booleanParam(name: 'Deploy', defaultValue: false, description: 'Toggle this value')
+        }
+            
+        stages{
+            stage('Clone code from GitHub'){
+                steps{
+                // git branch: 'main', url: 'https://github.com/srikanthhg/checkoutservice'
+                git url: 'https://github.com/srikanthhg/recommendationservice',  branch: 'main'
                 }
             }
-        }
-
-        stage(){
-            steps{
+            stage('Get the version'){
+                steps{
+                    script {
+                        def packageVersion = readFile('version.py').trim()
+                        if (packageVersion) {
+                            // Access the captured version group (group 1)
+                            //packageVersion = packageVersion[0][1]
+                            echo "Application version: $packageVersion"
+                        } else {
+                            error "Application version not found in main.go"
+                        }          
+                    }
+                }
             }
-        }
 
-        stage(){
-            steps{
+            stage(){
+                steps{
+                }
             }
+
+            stage(){
+                steps{
+                }
+            }
+
         }
 
-    }
-
-    post { 
-        always { 
-            echo 'I will always say Hello again!'
-            deleteDir()
-        }
-        failure { 
-            echo 'this runs when pipeline is failed, used generally to send some alerts'
-        }
-        success{
-            echo 'I will say Hello when pipeline is success'
+        post { 
+            always { 
+                echo 'I will always say Hello again!'
+                deleteDir()
+            }
+            failure { 
+                echo 'this runs when pipeline is failed, used generally to send some alerts'
+            }
+            success{
+                echo 'I will say Hello when pipeline is success'
+            }
         }
     }
 }
