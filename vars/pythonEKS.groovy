0e2229fd5b14c778990338009c425ced838f1a18
srikanthg <skanth306@gmail.com>
2024-05-26 16:28:01 +0530

    jenkins

--- vars/pythonEKS.groovy
+++ vars/pythonEKS.groovy
@@ -61,7 +61,7 @@ def call(Map configMap){
                 steps {
                     sh """
                         ls -ltr
-                        zip -q -r ${configMap.component}.zip ./* -x ".git" -x "*.zip"
+                        zip -q -r ${configMap.component}.zip . -x ".git" -x "*.zip"
                         ls -ltr
                         
                     
