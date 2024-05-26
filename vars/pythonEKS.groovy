baa8896e6a39ce284e4dbe69ee3734b1d6666aea
srikanthg <skanth306@gmail.com>
2024-05-26 16:41:12 +0530

    jenkins

--- vars/pythonEKS.groovy
+++ vars/pythonEKS.groovy
@@ -28,8 +28,10 @@ def call(Map configMap){
             stage('Get the version'){
                 steps{
                     script {
-                        def packageVersion = readFile('version.py').trim()
-                        if (packageVersion) {                       
+                        def versionContent = readFile('version.py').trim()
+                        def versionMatch = versionContent =~ /__version__ = "(.*)"/
+                        if (versionMatch) {   
+                            packageVersion = versionMatch[0][1]                    
                             echo "Application version: $packageVersion"
                         } else {
                             error "Application version not found in main.go"
