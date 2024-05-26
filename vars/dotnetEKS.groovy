724a54c1a6fac5db78ac48e6982328cba2227ee1
srikanthg <skanth306@gmail.com>
2024-05-26 11:57:55 +0530

    jenkins

--- vars/dotnetEKS.groovy
+++ vars/dotnetEKS.groovy
@@ -96,7 +96,7 @@ def call(Map configMap){
                         ls -ltr
                         zip -q -r ${configMap.component}.zip ./${configMap.component}/* -x ".git" -x "*.zip"
                         ls -ltr
-                    
+                        unzip -l cartservice.zip 
                     """
                 }
             }
