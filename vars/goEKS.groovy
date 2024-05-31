29917bb8c31bdad7f4828d0747c3ce946b86f593
srikanthg <skanth306@gmail.com>
2024-05-31 14:16:48 +0530

    jenkins

--- vars/goEKS.groovy
+++ vars/goEKS.groovy
@@ -95,8 +95,8 @@ def call(Map configMap){
                     spec: '''{
                         "files": [
                             {
-                            "pattern": '${configMap.component}.zip',
-                            "target": '${configMap.component}'
+                            "pattern": "frontend.zip",
+                            "target": "frontend"
                             }
                         ]
                     }'''
